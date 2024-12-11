ef1d7bd65d6134996e60f6635433f90d396a041d
Dzmitry Rymarau <github@dzmitryrymarau.com>
2024-12-11 11:42:38 +0300

    fixup! feat: day 10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -13,23 +13,23 @@ class Day10(private val input: Input) {
         var count = 0
         for (position in input.positions.filter { input[it] == '0' }) {
             val collection = block()
-            collection.count(position)
+            count(position, block = collection::add)
             count += collection.size
         }
         return count
     }
 
-    private fun MutableCollection<Position>.count(position: Position, current: Char = '0') {
+    private fun count(position: Position, current: Char = input[position], block: (Position) -> Unit) {
         if (input[position] != current) return
         if (current == '9') {
-            add(position)
+            block(position)
             return
         }
         val current = current.nextDigit()
         for (distance in distances) {
             val position = position + distance
             if (position !in input) continue
-            count(position, current)
+            count(position, current, block)
         }
     }
 
